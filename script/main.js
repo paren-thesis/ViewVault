b52934e11148fcfb8ac07f17cf2f4e327823dcce
p@ren <ntimprosper308@gmail.com>
2024-11-17 17:11:31 +0000

    Add fade animation

---
+++ script/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('scroll', () => {
+    const topic = document.querySelector('.topic');
+    const content = document.querySelector('.content');
+    const section = document.querySelector('.more-thumbnail');
+    const sectionTop = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight - 50) {
+        topic.style.opacity = '1';
+        content.style.opacity = '1';
+    }
+});
